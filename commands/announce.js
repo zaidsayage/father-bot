42bac87039ab8c45edc8e2897cb1bd35a8924ac1
Zaid <zwsayage@gmail.com>
2019-11-17 12:26:50 -0800

    switched ids

--- commands/announce.js
+++ commands/announce.js
@@ -1,19 +1,19 @@
 exports.run = (client,message,args, ops) => {
     const Discord = require('discord.js')
-    if(!args[0].join(" "))
+    if(!args[0])
     {
         message.reply("No message to announce.")
     }
-    else if (message.author.id !== ops.ownerID) {
+    else if (message.author.id !== ops.ownerID || message.author.id !== ops.adminID) {
         message.reply('Invalid Permissions.')
     }
     else {
     const announceEmbed = new Discord.RichEmbed()
     .setColor('#0000FF')
     .setAuthor("Announcement")
-    .setDescription(`${args[0]}`)
+    .setDescription(`${args.join(" ")}`)
     .setTimestamp()
     .setFooter(message.author.tag);
-    client.channels.get("645509843460947968").send(announceEmbed)
+    client.channels.get("427172543557009429").send(announceEmbed)
     }
 }
